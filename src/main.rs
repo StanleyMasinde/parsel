e4558d44d5034fdc626b77972aa2e745fd6005d7
Stanley Masinde <stanmasinde@gmail.com>
2025-12-12 02:33:35 +0300

    feat: add human-panic integration for user-friendly crash reports
    Added `human-panic` as a dependency and initialized it in `main.rs` with
    custom
    metadata (authors, homepage, support info). Updated `Cargo.toml` and
    `Cargo.lock` accordingly.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use human_panic::{Metadata, setup_panic};
 use parsel::ui;
 
 fn main() {
+    setup_panic!(
+        Metadata::new(env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"))
+            .authors("Stanley Masinde <hello@stanleymasinde.com>")
+            .homepage("github.com/StanleyMasinde/parsel")
+            .support("- Open a support request by email to hello@stanleymasinde.com")
+    );
+
     ui::run().unwrap();
 }
